ecca81a892dff34c2096c13cb507e2650eed2898
Tu Anh Huynh <tuanhlyhuynh@gmail.com>
2020-12-10 16:22:23 -0500

    [Pokedex] Add class component with states and api call

---
+++ src/components/Pokedex.js
@@ -0,0 +1,56 @@
+import React, { Component} from 'react'
+import Axios from 'axios'
+
+class Pokedex extends Component {
+    constructor () {
+        super ()
+        this.state = {
+            pokemonName: 'pikachu',
+            imageUrl: ''
+
+        }
+    }
+
+    componentDidMount() {
+        Axios.get(`https://pokeapi.co/api/v2/pokemon/${this.state.pokemonName}`)
+        .then((res) => {
+            this.setState({
+                imageUrl: res.data.sprites.other['official-artwork'].front_default
+            })
+
+        })
+
+    }
+    componentDidUpdate() {
+        if (this.state.pokemonName === '') {
+            return
+        }
+        Axios.get(`https://pokeapi.co/api/v2/pokemon/${this.state.pokemonName}`)
+        .then((res) => {
+            this.setState({
+                imageUrl: res.data.sprites.other['official-artwork'].front_default
+            })
+
+        }).catch((err) => {
+            this.setState({
+                imageUrl: ''
+            })
+        })
+
+    }
+
+    render () {
+        return (
+            <div>
+                <h1>My React Pokedex</h1>
+                <input value={this.state.pokemonName} onChange={(e) => {this.setState({pokemonName: e.target.value.toLowerCase()})}} />
+                <div>
+
+                <img src={this.state.imageUrl} />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Pokedex
